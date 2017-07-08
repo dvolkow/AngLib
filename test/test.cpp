637f1d6e58ae2d6ce1bc95fcc7c3d95e75153dd3
Daniel Wolkow <volkov12@rambler.ru>
2017-07-09 00:28:23 +0300

    add btl lib to test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,49 +1,17 @@
 #include "../debug/anglib.hpp"
-#include <cassert>
+#include <my_dev/btl.hpp>
 
 using anglib::Deg;
 using anglib::Hour;
 using anglib::Const;
 
+using btl::Test;
+
+Test t;
+
 int main()
 {
-	//--1. Конструкторы
-	Deg dg; 
-	Deg dg1(1);
-	Deg dg2(-1.5);
-	Deg dg3 = {3};
-	Deg dg4 = {3, 300, -100};
-
-	Hour hr;
-	Hour hr1(1);
-	Hour hr2(-1.5);
-	Hour hr3 = {3};
-	Hour hr4 = {3, 300, -100};
-
-	Deg dg_a(hr1);
-	assert(dg_a == hr1);
-
-	Hour hr_a(dg3);
-	assert(hr_a == dg3);
-
-	//--2. Приведения
-	short sh_a = hr_a;
-	int i_a = hr1;
-	long l_a = hr2;
-	long long ll_a = hr4;
-	float f_a = hr;
-
-	sh_a = dg3;
-	i_a = dg2;
-	l_a = dg;
-	ll_a = dg4;
-	f_a = dg2;
-
-	dg = sh_a;
-	dg = i_a;
-	dg = l_a;
-	dg = ll_a;
-	dg = f_a;
-
+	t.run<Deg>();
+	t.run<Hour>();
 	std::cout << "Smoke test passed!\n"; 
 }
