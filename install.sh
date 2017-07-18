4d8dc51e6e9e294db764f051ad78b307f0334244
Daniel Wolkow <volkov12@rambler.ru>
2017-07-18 19:32:12 +0300

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
-sudo mkdir /usr/include/my_dev
-sudo cp ./include/anglib.hpp /usr/include/my_dev/
+f=$1
+
+if ! [ -d /usr/include/my_dev ];
+then 
+	sudo mkdir /usr/include/my_dev
+fi
+
+if [ "$f" = "debug" ] 
+then 
+	echo "Установлена дебажная версия <my_dev/anglib.hpp>"
+	sudo cp ./debug/anglib.hpp /usr/include/my_dev/
+else
+	echo "Установлена релизная версия <my_dev/anglib.hpp>"
+	sudo cp ./include/anglib.hpp /usr/include/my_dev/
+fi
+
